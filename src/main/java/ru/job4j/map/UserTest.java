0f7b8f94f8a35e17b1dd61f0a12e26a67a7c039f
antonglavatskiy <glavazky@yandex.ru>
2022-06-10 11:30:54 +0500

    5. Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/job4j/map/UserTest.java
+++ src/main/java/ru/job4j/map/UserTest.java
@@ -10,9 +10,12 @@ public class UserTest {
         Calendar userBirthday = new GregorianCalendar(1990, 12, 31);
         User user1 = new User("User1", 1, userBirthday);
         User user2 = new User("User1", 1, userBirthday);
+        System.out.println("user1: " + user1);
+        System.out.println("user2: " + user2);
         System.out.println(bucket(user1) == bucket(user2));
         Map<User, Object> users = new HashMap<>();
         users.put(user1, new Object());
+        System.out.println("value1: " + users.get(user1));
         users.put(user2, new Object());
         for (Map.Entry<User, Object> user : users.entrySet()) {
             System.out.println(user);
