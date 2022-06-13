afb22ec3cf0a17cabddcf346221a201436770646
antonglavatskiy <glavazky@yandex.ru>
2022-06-13 12:20:47 +0500

    2. Добавить метод boolean isBinary() [#1712]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -6,6 +6,37 @@ import static org.junit.Assert.*;
 
 public class SimpleTreeTest {
     @Test
+    public void whenBinaryThenTrue() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(4, 11);
+        tree.add(3, 6);
+        tree.add(3, 7);
+        tree.add(6, 8);
+        tree.add(6, 9);
+        tree.add(7, 10);
+        assertTrue(tree.isBinary());
+    }
+
+    @Test
+    public void whenNotBinaryThenFalse() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(2, 6);
+        tree.add(5, 7);
+        tree.add(5, 8);
+        tree.add(3, 9);
+        tree.add(3, 10);
+        assertFalse(tree.isBinary());
+    }
+
+    @Test
     public void when6ElFindLastThen6() {
         Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
