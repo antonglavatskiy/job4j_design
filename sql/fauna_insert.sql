7a8d0c65377560913b4610d0294c6341523820d9
antonglavatskiy <glavazky@yandex.ru>
2022-07-09 15:50:14 +0500

    0. Select [#6861]

---
+++ sql/fauna_insert.sql
@@ -0,0 +1,5 @@
+insert into fauna (name, avg_age, discovery_date) values ('goldfish', 1200, '01.31.1800');
+insert into fauna (name, avg_age, discovery_date) values ('elephant', 7200, '03.03.1750');
+insert into fauna (name, avg_age, discovery_date) values ('wolf', 4250, '03.03.1300');
+insert into fauna (name, avg_age, discovery_date) values ('turtle', 15000, '03.06.1779');
+insert into fauna (name, avg_age, discovery_date) values ('fox', 7200, '12.21.1246');
