d02aad375d7dbbcb587f3868c27b39132fa69b52
antonglavatskiy <glavazky@yandex.ru>
2022-09-10 14:06:25 +0500

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851]

---
+++ src/main/java/ru/job4j/design/srp/XMLReportEngine.java
@@ -0,0 +1,33 @@
+package ru.job4j.design.srp;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.function.Predicate;
+
+public class XMLReportEngine implements Report {
+    private Store store;
+
+    public XMLReportEngine(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public String generate(Predicate<Employee> filter) {
+        String xml = "";
+        try {
+            JAXBContext context = JAXBContext.newInstance(Employees.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            try (StringWriter writer = new StringWriter()) {
+                marshaller.marshal(new Employees(store.findBy(filter)), writer);
+                xml = writer.getBuffer().toString();
+            }
+        } catch (IOException | JAXBException e) {
+            e.printStackTrace();
+        }
+        return xml;
+    }
+}
--- src/test/java/ru/job4j/design/srp/ReportEngineTest.java
+++ src/test/java/ru/job4j/design/srp/ReportEngineTest.java
@@ -2,12 +2,19 @@ package ru.job4j.design.srp;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.*;
 import static ru.job4j.design.srp.ReportEngine.DATE_FORMAT;
 
 class ReportEngineTest {
+    public static final SimpleDateFormat XML_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    public static final String TAB = "    ";
+    public static final String ENTER = System.lineSeparator();
+    public static final String DATE_ELEMENT = "{\"%s\":%d,\"%s\":%d,\"%s\":%d,\"%s\":%d,\"%s\":%d,\"%s\":%d},";
+
     @Test
     public void whenOldGenerated() {
         MemStore memStore = new MemStore();
@@ -17,12 +24,12 @@ class ReportEngineTest {
         Report engine = new ReportEngine(memStore);
         StringBuilder expect = new StringBuilder()
                 .append("Name; Hired; Fired; Salary;")
-                .append(System.lineSeparator())
+                .append(ENTER)
                 .append(employee.getName()).append(";")
                 .append(DATE_FORMAT.format(employee.getHired().getTime())).append(";")
                 .append(DATE_FORMAT.format(employee.getFired().getTime())).append(";")
                 .append(employee.getSalary()).append(";")
-                .append(System.lineSeparator());
+                .append(ENTER);
         assertThat(engine.generate(emp -> true)).isEqualTo(expect.toString());
     }
 
@@ -64,12 +71,12 @@ class ReportEngineTest {
         Report engine = new AccountantReportEngine(memStore);
         StringBuilder expect = new StringBuilder()
                 .append("Name; Hired; Fired; Extra payment;")
-                .append(System.lineSeparator())
+                .append(ENTER)
                 .append(employee.getName()).append(";")
                 .append(DATE_FORMAT.format(employee.getHired().getTime())).append(";")
                 .append(DATE_FORMAT.format(employee.getFired().getTime())).append(";")
                 .append(employee.getSalary() * AccountantReportEngine.PERCENT).append(";")
-                .append(System.lineSeparator());
+                .append(ENTER);
         assertThat(engine.generate(emp -> true)).isEqualTo(expect.toString());
     }
 
@@ -86,17 +93,71 @@ class ReportEngineTest {
         Report engine = new HRReportEngine(memStore);
         StringBuilder expect = new StringBuilder()
                 .append("Name; Salary;")
-                .append(System.lineSeparator())
+                .append(ENTER)
                 .append(employee3.getName()).append(";")
                 .append(employee3.getSalary()).append(";")
-                .append(System.lineSeparator())
+                .append(ENTER)
                 .append(employee1.getName()).append(";")
                 .append(employee1.getSalary()).append(";")
-                .append(System.lineSeparator())
+                .append(ENTER)
                 .append(employee2.getName()).append(";")
                 .append(employee2.getSalary()).append(";")
-                .append(System.lineSeparator());
+                .append(ENTER);
         assertThat(engine.generate(emp -> true)).isEqualTo(expect.toString());
     }
 
+    @Test
+    public void whenGeneratedXMLReport() {
+        MemStore memStore = new MemStore();
+        Calendar date = Calendar.getInstance();
+        Employee employee = new Employee("Maxim", date, date, 700);
+        memStore.add(employee);
+        Report engine = new XMLReportEngine(memStore);
+        StringBuilder expect = new StringBuilder()
+                .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
+                .append(ENTER)
+                .append("<employees>").append(ENTER)
+                .append(TAB).append("<employees>").append(ENTER)
+                .append(TAB).append(TAB).append("<fired>")
+                .append(XML_FORMAT.format(employee.getFired().getTime())).append("</fired>")
+                .append(ENTER).append(TAB).append(TAB).append("<hired>")
+                .append(XML_FORMAT.format(employee.getHired().getTime())).append("</hired>")
+                .append(ENTER).append(TAB).append(TAB)
+                .append("<name>").append(employee.getName()).append("</name>")
+                .append(ENTER).append(TAB).append(TAB)
+                .append("<salary>").append(employee.getSalary()).append("</salary>")
+                .append(ENTER).append(TAB).append("</employees>")
+                .append(ENTER).append("</employees>").append(ENTER);
+        assertThat(engine.generate(emp -> true)).isEqualTo(expect.toString());
+    }
+
+    @Test
+    public void whenGeneratedJSONReport() {
+        MemStore memStore = new MemStore();
+        Calendar date = Calendar.getInstance();
+        Employee employee = new Employee("Anna", date, date, 500);
+        memStore.add(employee);
+        Report engine = new JSONReportEngine(memStore);
+        StringBuilder expect = new StringBuilder()
+                .append("[{")
+                .append("\"name\":").append(String.format("\"%s\",", employee.getName()))
+                .append("\"hired\":")
+                .append(String.format(DATE_ELEMENT,
+                        "year", employee.getHired().get(Calendar.YEAR),
+                        "month", employee.getHired().get(Calendar.MONTH),
+                        "dayOfMonth", employee.getHired().get(Calendar.DAY_OF_MONTH),
+                        "hourOfDay", employee.getHired().get(Calendar.HOUR_OF_DAY),
+                        "minute", employee.getHired().get(Calendar.MINUTE),
+                        "second", employee.getHired().get(Calendar.SECOND)))
+                .append("\"fired\":")
+                .append(String.format(DATE_ELEMENT,
+                        "year", employee.getFired().get(Calendar.YEAR),
+                        "month", employee.getFired().get(Calendar.MONTH),
+                        "dayOfMonth", employee.getFired().get(Calendar.DAY_OF_MONTH),
+                        "hourOfDay", employee.getFired().get(Calendar.HOUR_OF_DAY),
+                        "minute", employee.getFired().get(Calendar.MINUTE),
+                        "second", employee.getFired().get(Calendar.SECOND)))
+                .append("\"salary\":").append(String.format(Locale.ROOT, "%.1f}]", employee.getSalary()));
+        assertThat(engine.generate(emp -> true)).isEqualTo(expect.toString());
+    }
 }
