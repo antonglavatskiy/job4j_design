a9b33cc19c033fcec0d67468cb47c3f561aadd83
antonglavatskiy <glavazky@yandex.ru>
2023-01-20 17:32:07 +0500

    1. String pool, Integer pool [#504931]

---
+++ src/test/java/ru/job4j/pool/StringPoolExample5.java
@@ -0,0 +1,10 @@
+package ru.job4j.pool;
+
+public class StringPoolExample5 {
+    public static void main(String[] args) {
+        System.out.println(new String("New string")
+                == new String("New string"));
+        System.out.println(new String("Interned string").intern()
+                == new String("Interned string").intern());
+    }
+}
